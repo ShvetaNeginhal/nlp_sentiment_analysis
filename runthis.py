0b798a30dd3579997f531e5a78c7fa5ffcae87d6
shwetha <saneginhal@gmail.com>
2018-04-03 16:44:38 +0530

    first

---
+++ runthis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import sentimental as s
+
+
+print(s.sentiment("I'm depressed and sad. I have got no happiness in life."))
+print(s.sentiment("I have no regrets. I am totally happy in my life"))
